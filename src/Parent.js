718d6cbd0924ad8bf62fc5226f2195ab30539494
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-01 16:38:28 -0600

    completed lab

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,24 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childColor, setChildColor] = useState("#FFF")
+
+  function handleColor() {
+    const randomColor = getRandomColor();
+    setColor(randomColor)
+    setChildColor(getRandomColor())
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child 
+        function = {handleColor}
+        childColor = {childColor}
+       />
+      <Child 
+       function = {handleColor}
+       childColor = {childColor}
+       />
     </div>
   );
 }
